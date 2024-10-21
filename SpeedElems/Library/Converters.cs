cc93e54877fe9338441e7a2e31181fb721cf862b
SpaaJ <3485946+SpaaJ@users.noreply.github.com>
2024-10-21 11:46:35 +0200

    Update achievements

--- SpeedElems/Library/Converters.cs
+++ SpeedElems/Library/Converters.cs
@@ -1,16 +1,18 @@
 ﻿using System.Globalization;
-
-//using CommunityToolkit.Converters;
+using CommunityToolkit.Maui.Converters;
 
 namespace SpeedElems.Library.Converters;
 
-//public class ZeroToBoolConverter : BaseConverterOneWay<int, bool>
-//{
-//    public override bool ConvertFrom(int value, CultureInfo? culture = null) => value is 0;
+public class ZeroToBoolConverter : BaseConverterOneWay<int, bool>
+{
+    public override bool DefaultConvertReturnValue { get; set; }
 
-//}
+    public override bool ConvertFrom(int value, CultureInfo? culture = null) => value is 0;
+}
 
-//public class GreaterOrEqualThanParameterToBoolConverter : BaseConverterOneWay<int, bool, string>
-//{
-//    public override bool ConvertFrom(int value, string parameter, CultureInfo? culture = null) => value >= int.Parse(parameter);
-//}
+public class GreaterOrEqualThanParameterToBoolConverter : BaseConverterOneWay<int, bool, string>
+{
+    public override bool DefaultConvertReturnValue { get; set; }
+
+    public override bool ConvertFrom(int value, string parameter, CultureInfo? culture = null) => value >= int.Parse(parameter);
+}
